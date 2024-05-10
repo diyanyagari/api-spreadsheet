875caba62f43efa340127fbf075fbaa4ea7daab1
Diyan Yagari Pratama <diyan.pratama@autopedia.id>
2024-05-10 11:00:00 +0700

    fixing some problems

--- api/index.ts
+++ api/index.ts
@@ -4,43 +4,52 @@ import sheets from "./sheetClient";
 
 const app = express();
 
-const contactFormSchema = z.object({
-  name: z.string().min(1, { message: "Name is required" }),
-  email: z.string().email(),
-  message: z.string().min(1, { message: "Message is required" }),
-});
+const contactFormSchema = z.array(
+  z.object({
+    date: z.string(),
+    employeeName: z.string().min(1, { message: "Employee Name is required" }),
+    shift: z.string().min(1, { message: "Shift is required" }),
+    analisa: z.string().min(1, { message: "Analisa is required" }),
+    idTiket: z.string().min(1, { message: "ID Tiket is required" }),
+  })
+);
 
 app.use(express.json());
+app.use(express.static("public"));
 
-app.post("/send-message", async (req: any, res: any) => {
-    try {
-      const body = contactFormSchema.parse(req.body);
+app.post("/shift-data", async (req: any, res: any) => {
+  try {
+    const body = contactFormSchema.safeParse(req.body);
+
+    let rowData: string[][] = [];
+    
+    if (body.success) {
+      rowData = body.data.map((item) => Object.values(item));
+
       await sheets.spreadsheets.values.append({
         spreadsheetId: process.env.SHEET_ID,
-        range: "Sheet1!A2:C",
+        range: "Form Responses 1!A2:E",
         insertDataOption: "INSERT_ROWS",
         valueInputOption: "RAW",
-        requestBody: {
-          values: [
-            ["hehe", "hehe", "hehe"],
-            ["hehe", "hehe", "hehe"],
-          ],
-        },
+        requestBody: { values: rowData },
       });
-      res.json({ message: "Data added successfully" });
-    } catch (error) {
-      if (error instanceof ZodError) {
-        res.status(400).json({ error: error.message });
-      } else {
-        console.log(error)
-        res.status(400).json({ error });
-      }
+
+      res.json({ message: "Data added successfully", statusCode: 200 });
+    } else {
+      res.status(400).json({ message: "Invalid Format Data", statusCode: 400 });
+      console.error("Validation errors:", body.error.errors);
     }
-  });
+  } catch (error) {
+    if (error instanceof ZodError) {
+      res.status(400).json({ error: error.message });
+    } else {
+      res.status(400).json({ error });
+    }
+  }
+});
 
 app.get("/", (req: any, res: any) => res.send("Express on Vercel"));
 
 app.listen(4000, () => console.log("Server ready on port 4000."));
-
 
 module.exports = app;
